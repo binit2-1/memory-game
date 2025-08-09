465423c82d03d9173d2b00b59bc9df86a3642d01
binit <binitgupta.1711@gmail.com>
2025-08-09 23:22:23 +0530

    minor margin changes

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -4,7 +4,7 @@ const HeroText = () => {
   return (
     <div className='flex flex-col items-center justify-center'>
         <img src='/logo.png' alt='Logo' className='w-[700px] h-auto'/>
-        <h1 className='font-title text-[90px] drop-shadow-[4px_4px_0px_#c026d3] text-yellow-300 my-10'>Memory Game</h1>
+        <h1 className='font-title text-[90px] drop-shadow-[4px_4px_0px_#c026d3] text-yellow-300 -my-5'>Memory Game</h1>
     </div>
   )
 }
