ae9e19ed29067bc548472819fccb4108c9fff782
binit <binitgupta.1711@gmail.com>
2025-08-09 12:05:57 +0530

    -Added a morty logo on the tab -Added a Rick and Morty Text logo at the lop of the home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useState, useRef, useEffect } from 'react'
 import { Github, Volume2, VolumeOff, CircleQuestionMark, SendToBack } from 'lucide-react'
-import CircularButton from './components/CircularButton.jsx'
+import CircularButton from './components/CircularButton.jsx'  
 
 const App = () => {
   const [isMuted, setIsMuted] = useState(false)
   const audioRef = useRef(null)
-  useEffect(() => {
-    if (audioRef.current){
-      audioRef.current.play().catch((error) => {
-        console.error("Error playing audio:", error)
-      })
-    }
-  }, [])
+useEffect(() => {
+  if (audioRef.current){
+    audioRef.current.play().catch((error) => {
+      console.error("Error playing audio:", error)
+    })
+  }
+}, [])   
 
   function handleVolumeToggle() {
     setIsMuted(!isMuted)
@@ -29,6 +29,7 @@ const App = () => {
     window.open('https://github.com/binit2-1/memory-game')
   }
 
+
   return (
     <div className="relative w-screen h-screen">
       <video
@@ -36,12 +37,16 @@ const App = () => {
         loop
         muted
         className="absolute w-screen h-screen object-cover"
+        onError={(e) => console.error("Error loading video:", e)}
       >
         <source src="/background.mp4" type="video/mp4" />
+        <source src="/background.webm" type="video/webm" />
       </video>
       <audio ref={audioRef} src='./theme.mp3' loop preload='auto' />
+      <div className='absolute flex items-start justify-start w-[200px] h-auto top-8 left-[13rem]'>
+        <img src='/logo.png' alt='Logo' />
+      </div>
       <div className="absolute flex items-center justify-between bottom-8 w-full px-52">
-
         <div className="flex gap-4">
           <CircularButton icon={<CircleQuestionMark />} />
           <CircularButton icon={isMuted ? <VolumeOff /> : <Volume2 />} onClick={handleVolumeToggle} />
