f5edee7b87c81623f8dc3b4b5aedd82002a61188
binit <binitgupta.1711@gmail.com>
2025-08-24 22:33:49 +0530

    MEdium and Hard mode added

---
+++ src/pages/GameBoardMed.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import fetchCharacters from '../api/rickAndMorty.js'
+import { useState, useEffect } from 'react'
+import Scorecard from '../components/Scorecard.jsx'
+import FlipCard from '../components/FlipCard.jsx'
+
+const GameBoard = () => {
+  const [char, setChar] = useState([])
+
+  useEffect(() => {
+    async function loadCharacters() {
+      const characters = await fetchCharacters()
+      console.log('Characters loaded:', characters)
+      setChar(characters.slice(0, 4))
+    }
+    
+    loadCharacters()
+  }, [])
+
+  return (
+    <div className='relative w-full h-full'>
+      <div className='absolute top-8 right-8 z-50'>
+        <Scorecard />
+      </div>
+      <div className='flex justify-center items-center h-full gap-6 p-8'>
+        {char.map((character, index) => ( 
+          <FlipCard 
+            key={character.id || index} 
+            character={character.image} 
+            name={character.name}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default GameBoard
